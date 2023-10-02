dd0b16f56f0e8ea40cba0a72bceacbff094b8a37
Sxch1n <008436812@coyote.csusb.edu>
2023-10-02 12:24:50 -0700

    ContactUs button

---
+++ Team-Cat/Assets/Scripts/ContactUs.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void SwitchToContactUs()
+    {
+        
+        SceneManager.LoadScene("ContactUs");
+    }
+    public void SwitchToHome()
+    {
+
+        SceneManager.LoadScene("HomeScene");
+    }
+}
