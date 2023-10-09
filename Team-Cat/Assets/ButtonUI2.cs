b59030a4d05ed255bf8ab73299e37e07129fd496
AlphaLearner <86737329+AlphaLearner@users.noreply.github.com>
2023-10-08 23:33:54 -0700

    change

--- Team-Cat/Assets/ButtonUI2.cs
+++ Team-Cat/Assets/ButtonUI2.cs
@@ -1,70 +1,49 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
-/*using static PlasticGui.LaunchDiffParameters;*/
+using UnityEngine.Networking;
+using System.Collections;
 
 public class ButtonUI2 : MonoBehaviour
 {
-    [Header("Azure DevOps Configuration")]
-    public string personalAccessToken = "32wfcjsnq7azzghj4rdmbsxsfoidncbb2qmxbg7ntwii3ks6xvba"; // Replace with your personal access token.
-    public string organizationName = "008438216"; // Replace with your Azure DevOps organization name.
-    public string project = "Team-Cat"; // Replace with your Azure DevOps project name.
-    private int buildDefinitionId = _build; // Replace with the ID of your build definition.
+    public Text responseText;
 
-    [Header("UI Elements")]
-    public Button buildButton;
-    public Text statusText;
-
-    private const string DevOpsApiUrl = "https://dev.azure.com/008438216/";
-    private static readonly int _build;
-
-    void Start()
+    [System.Obsolete]
+    public void SendHelloWorldMessage()
     {
-        buildButton.onClick.AddListener(Update);
+        string message = "Hello World";
+
+        StartCoroutine(SendMessageToAzureDevOpsAPI(message));
     }
 
-    // Update is called once per frame
-    async void Update()
+    [System.Obsolete]
+    private IEnumerator SendMessageToAzureDevOpsAPI(string message)
     {
-        statusText.text = "Triggering build...";
+        // Replace with your Azure DevOps API endpoint and Personal Access Token
+        string devOpsUrl = "https://dev.azure.com/008438216/Team-Cat";
+        string personalAccessToken = "cwjtp43gmlnlyc6x7db2otngmmfvubwh6xzz3i6znwtswgufy4pq";
 
-        try
+        // Create a JSON payload with the message
+        string jsonPayload = "{\"message\": \"" + message + "\"}";
+
+        UnityWebRequest request = new UnityWebRequest(devOpsUrl, "POST");
+        byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(jsonPayload);
+
+        request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+        request.SetRequestHeader("Authorization", "Basic " + System.Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes(":" + personalAccessToken)));
+
+        yield return request.SendWebRequest();
+
+        if (request.isNetworkError || request.isHttpError)
         {
-            string requestUrl = string.Format(DevOpsApiUrl, organizationName, project);
-
-            using (HttpClient client = new HttpClient())
-            {
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes(string.Format("{0}:{1}", "", personalAccessToken))));
-
-                var requestBody = new
-                {
-                    definition = new { id = buildDefinitionId }
-                };
-
-                var content = new StringContent(JsonUtility.ToJson(requestBody), System.Text.Encoding.UTF8, "application/json");
-
-                var response = await client.PostAsync(requestUrl, content);
-
-                if (response.IsSuccessStatusCode)
-                {
-                    statusText.text = "Build triggered successfully!";
-                }
-                else
-                {
-                    statusText.text = "Failed to trigger build. Check your Azure DevOps configuration.";
-                }
-            }
+            Debug.LogError("Error sending message to Azure DevOps: " + request.error);
+            responseText.text = "Error: " + request.error;
         }
-        catch (Exception ex)
+        else
         {
-            statusText.text = "An error occurred: " + ex.Message;
+            Debug.Log("Message sent successfully to Azure DevOps");
+            responseText.text = "Message sent successfully!";
         }
     }
 }
-
