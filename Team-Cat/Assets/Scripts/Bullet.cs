41b7fb02a0ebde7d37f7a1aded6f3e9ad99fc6f2
Sxch1n <008436812@coyote.csusb.edu>
2023-10-14 12:26:04 -0700

    retry switcher

--- Team-Cat/Assets/Scripts/Bullet.cs
+++ Team-Cat/Assets/Scripts/Bullet.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Bullet : MonoBehaviour
 {
@@ -18,6 +19,8 @@ public class Bullet : MonoBehaviour
         if(hitInfo.gameObject.tag.Equals("Monster"))
         {
             Destroy(hitInfo.gameObject);
+            //SceneManager.LoadScene("Retry");
+
         }
     }
 }
