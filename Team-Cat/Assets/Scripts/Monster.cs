c8e899212b9945bc6ae72950db629d3867c6f544
Fahim Fayaz <fahimfayaz18@gmail.com>
2023-10-29 22:51:13 -0700

    feat: add animations

--- Team-Cat/Assets/Scripts/Monster.cs
+++ Team-Cat/Assets/Scripts/Monster.cs
@@ -5,13 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class Monster : MonoBehaviour
 {
+    public Animator animator;
     private float _speed = 2f;
+    public string Retry= "Retry";
+    public float delay = 3;
     
 
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {	
     }
 
     // Update is called once per frame
@@ -25,16 +27,23 @@ public class Monster : MonoBehaviour
         // Check if the object collided with a stationary object
         if (collision.gameObject.CompareTag("Cat"))
         {
+            animator.SetTrigger("monsterStop");
             transform.Translate(Vector3.left * (Time.deltaTime * 0));
             Debug.Log("coliision!!");
-            LoadNewScene();
+            StartCoroutine(LoadLevelAfterDelay(delay)); 
+            //LoadNewScene();
         }
     }
 
-    private void LoadNewScene()
-    {
-        SceneManager.LoadScene("Retry");
+    // private void LoadNewScene()
+    // {
+    //     SceneManager.LoadScene("Retry");
        
+    // }
+    IEnumerator LoadLevelAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(Retry);
     }
 }
 
--- Team-Cat/Assets/Scripts/Weapon.cs
+++ Team-Cat/Assets/Scripts/Weapon.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Weapon : MonoBehaviour
 {
     public Animator animator;
@@ -22,5 +23,15 @@ public class Weapon : MonoBehaviour
     {
         animator.SetTrigger("shoot");
         Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if the object collided with a stationary object
+        if (collision.gameObject.CompareTag("Monster"))
+        {
+            animator.SetTrigger("dead");
+            Debug.Log("coliision!!");
+        }
     }
 }
