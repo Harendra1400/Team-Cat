f8e9a790a928583932b05ffbe086bdd55580242b
Fahim Fayaz <fahimfayaz18@gmail.com>
2023-11-06 19:53:55 -0800

    feat: add boss animation, audio

---
+++ Team-Cat/Assets/Scripts/kingpinScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kingpinScript : MonoBehaviour
+{
+    [SerializeField] private AudioSource MonsterHitEffect;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D (Collider2D hitInfo)
+    {
+        //Destroy(gameObject);
+        if(hitInfo.CompareTag("Bullet"))
+        {
+            Destroy(hitInfo.gameObject);//destroy bullet
+            MonsterHitEffect.Play();
+            animator.SetTrigger("BossExplode");
+            Invoke("Delaydestroy", 1.0f);
+            
+        }
+    }
+    private void Delaydestroy()
+    {
+        Destroy(gameObject);
+    }
+}
